8762a09d498a4c3d078694710b09cb4e4a4cb3e9
Victoria Goliber <vgoliber@dwavesys.com>
2021-05-04 13:08:14 +0200

    Update loop index names for consistency

--- demo.py
+++ demo.py
@@ -179,10 +179,10 @@ def build_dqm(W, C, n, p, a, verbose=True):
 
     # Constraint: Exactly p hubs required.
     gamma2 = 250
-    for j in range(n):
-        dqm.set_linear_case(j, j, dqm.get_linear_case(j,j) + (1-2*p)*gamma2)
-        for k in range(j+1,n):
-            dqm.set_quadratic_case(j, j, k, k, dqm.get_quadratic_case(j, j, k, k) + 2*gamma2)
+    for i in range(n):
+        dqm.set_linear_case(i, i, dqm.get_linear_case(i,i) + (1-2*p)*gamma2)
+        for j in range(i+1,n):
+            dqm.set_quadratic_case(i, i, j, j, dqm.get_quadratic_case(i, i, j, j) + 2*gamma2)
 
     return dqm
 
